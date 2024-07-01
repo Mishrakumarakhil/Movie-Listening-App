a8ae8a41e3e7e1df852d1abf19880a4c4ad223ee
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-07-01 23:22:09 +0530

    Added action function for getting movie data

--- src/components/MovieListPage.js
+++ src/components/MovieListPage.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./MovieListPage.css";
 import { Header } from "./Header";
 import { Input } from "./Input";
+import { fetchMovieList } from "../helper/action";
 
 export const MovieListPage = () => {
   const [searchValue, setSearchValue] = useState("");
   const handleInputChange = (event) => {
     setSearchValue(event.target.value);
   };
+
+  useEffect(() => {
+    fetchMovieList();
+  }, []);
   return (
     <>
       <Header />
