3fb5a8829f8f6eb174abcfe33841539b7b52b00e
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-07-01 22:54:27 +0530

    Added herader component with nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
 import React from "react";
 import "./Header.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { NavBar } from "./NavBar";
+import { MovieListPage } from "./MovieListPage";
 export const Header = () => {
   return (
-    <div>
-      <div>Movie List</div>
+    <div className="header-container">
+      <div className="logo-text">Movie List</div>
+      <div className="nav-bar">
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/home" element={<MovieListPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 };
