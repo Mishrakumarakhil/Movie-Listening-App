c9dc91734f82d9a2c5a18230eebd76a7d19b5a09
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-07-01 23:15:02 +0530

    added search box

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./Input.css";
 
-export const Input = () => {
-  return <div>Input</div>;
+export const Input = ({ value, handleInputChange }) => {
+  return (
+    <div className="search-box">
+      <input
+        type="text"
+        placeholder="Search..."
+        value={value}
+        onChange={handleInputChange}
+      />
+    </div>
+  );
 };
--- src/components/MovieListPage.js
+++ src/components/MovieListPage.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MovieListPage.css";
 import { Header } from "./Header";
+import { Input } from "./Input";
 
 export const MovieListPage = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const handleInputChange = (event) => {
+    setSearchValue(event.target.value);
+  };
   return (
     <>
       <Header />
+      <Input value={searchValue} handleInputChange={handleInputChange} />
     </>
   );
 };
